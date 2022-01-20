66a5bd4f668be580da17c54186a511b3119346a5
classtahl <94368329+classtahl@users.noreply.github.com>
2022-01-20 09:55:39 +0100

    Update icaupslookup.js

--- icaupslookup.js
+++ icaupslookup.js
@@ -2,14 +2,14 @@ var request = require('request');
 const fs = require('fs');
 
 var options = {
-    url: 'https://handla.api.ica.se/api/upclookup?upc=7310751163903' /// Streckkod
+    url: 'https://handla.api.ica.se/api/upclookup?upc=7310751163903' /// Streckkod på vara som ska kollas upp
 };
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body);
         let data = JSON.stringify(body);
-        fs.writeFileSync('./artikel.json',data);
+        fs.writeFileSync('./artikel.json',data);  // Sparar den hämtade artikel till en json file
 
     }
 }
